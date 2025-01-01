cb9cffb09157cc664b87ead7707e320f218ef0a7
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2025-01-01 15:39:10 +0300

    Update WebRaider.java

--- src/com/webraider/master/WebRaider.java
+++ src/com/webraider/master/WebRaider.java
@@ -18,24 +18,27 @@ public class WebRaider {
     private static String postData = "key1=value1&key2=value2";
     private static final AtomicInteger successCount = new AtomicInteger(0);
 
-    public static final String RESET = "\u001B[0m";
-
-    public static final String GREEN = "\u001B[32m";
+    public static final String RESET = "\\u001B[0m";
+    public static final String GREEN = "\\u001B[32m";
 
     public static void main(String[] args) {
+        try {
+            InputHandler.handleInput(args);
 
-        InputHandler.handleInput(args);
+            targetUrl = InputHandler.getTargetUrl();
+            numberOfThreads = InputHandler.getNumberOfThreads();
+            requestsPerThread = InputHandler.getRequestsPerThread();
+            delay = InputHandler.getDelay();
+            requestMethod = InputHandler.getRequestMethod();
+            postData = InputHandler.getPostData();
 
-        targetUrl = InputHandler.getTargetUrl();
-        numberOfThreads = InputHandler.getNumberOfThreads();
-        requestsPerThread = InputHandler.getRequestsPerThread();
-        delay = InputHandler.getDelay();
-        requestMethod = InputHandler.getRequestMethod();
-        postData = InputHandler.getPostData();
+            ThreadManager.executeRequests(numberOfThreads, requestsPerThread, delay, requestMethod, postData, successCount);
 
-        ThreadManager.executeRequests(numberOfThreads, requestsPerThread, delay, requestMethod, postData, successCount);
-
-        System.out.println(GREEN + "[INFO]" + RESET + " All tasks completed.");
-        System.out.printf(GREEN + "[INFO]" + RESET + " Total successful requests: %d%n", successCount.get());
+            System.out.println(GREEN + "[INFO]" + RESET + " All tasks completed.");
+            System.out.printf(GREEN + "[INFO]" + RESET + " Total successful requests: %d%n", successCount.get());
+        } catch (Exception e) {
+            System.err.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
